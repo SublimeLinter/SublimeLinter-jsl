9d99df8e7c041739f6131ad01bc0e3ac381393dd
Koen Lageveen <koen.lageveen@me.com>
2018-03-26 10:25:21 +0200

    config file no longer supported

--- linter.py
+++ linter.py
@@ -3,7 +3,6 @@ from SublimeLinter.lint import Linter, util
 
 class JSL(Linter):
     cmd = 'jsl -stdin -nologo -nosummary'
-    config_file = ('-conf', 'jsl.conf', '~')
     regex = r'''(?xi)
         # First line is (lineno): type: error message
         ^\((?P<line>\d+)\):.*?(?:(?P<warning>warning)|(?P<error>error)):\s*(?P<message>.+)$\r?\n
